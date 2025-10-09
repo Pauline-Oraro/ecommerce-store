717a4472e45cfd6974dc40ff4ab90bb71db70b12
Pauline-Oraro <hiltrapauline@gmail.com>
2025-10-09 11:58:23 +0300

    initialize server with express, middleware, and environment configuration

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+//load environment variables from .env file
+dotenv.config();
+
+//initialize express
+const app = express();
+const PORT = process.env.PORT;;
+
+//json middleware for parsing JSON request bodies
+app.use(express.json());
+
+//cors middleware for handling Cross-Origin Resource Sharing
+app.use(cors());
+
+//helmet middleware for security by setting various HTTP headers
+app.use(helmet());
+
+//morgan middleware for logging HTTP requests
+app.use(morgan("dev"));
+
+//listen for requests
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+})

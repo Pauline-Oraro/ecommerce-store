e1d0b30cdd8bfb4fc183eb4db47874f4216245db
Pauline-Oraro <hiltrapauline@gmail.com>
2025-10-11 20:32:50 +0300

    setting the themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import HomePage from "./pages/HomePage"
+import ProductPage from "./pages/ProductPage"
+import { useThemeStore } from "./store/useThemeStore"
 
 
 function App() {
+  const {theme} = useThemeStore()
   return(
-    <div data-theme="retro">
-      <h1 className="text-2xl text-amber-900">  Ecommerce store</h1>
+    <div className="min-h-screen bg-base-200 transition-colors duration-300" data-theme={theme}>
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/product/:id" element={<ProductPage/>}/>
+      </Routes>
 
     </div>
     

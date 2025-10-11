ac7610242a65d025960c823a01678f77a36434eb
Pauline-Oraro <hiltrapauline@gmail.com>
2025-10-11 16:37:57 +0300

    frontend react setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return(
+    <div data-theme="retro">
+      <h1 className="text-2xl text-amber-900">  Ecommerce store</h1>
+
+    </div>
+    
+  )
+}
+
+export default App

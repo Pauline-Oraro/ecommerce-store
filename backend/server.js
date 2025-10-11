e419d914edaa18d250983a211fd5801899a1556e
Pauline-Oraro <hiltrapauline@gmail.com>
2025-10-11 14:13:33 +0300

    implementing arcjet

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
 //load environment variables from .env file
 dotenv.config();
@@ -24,6 +25,37 @@ app.use(helmet());
 
 //morgan middleware for logging HTTP requests
 app.use(morgan("dev"));
+
+//apply arcjet limiting to all routes
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, // specifies that each request consumes 1 token
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too Many Requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
+      }
+      return;
+    }
+
+    // check for spoofed bots
+    if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
+    next(error);
+  }
+});
 
 //import routes
 app.use('/api/products', productRoutes)

8d36987ac63b52a25ff6c556a5e39f135388b96c
Pauline-Oraro <hiltrapauline@gmail.com>
2025-10-13 16:20:29 +0300

    implementing the delete product function

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,7 +1,9 @@
 import { EditIcon, Trash2Icon } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useProductStore } from "../store/useProductStore";
 
 const ProductCard = ({product}) => {
+    const {deleteProduct} = useProductStore();
   return (
     <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-300">
       <figure className="relative pt-[56.25%]">
@@ -23,7 +25,7 @@ const ProductCard = ({product}) => {
 
           <button
             className="btn btn-sm btn-error  btn-outline"
-            
+            onClick={() =>deleteProduct(product.id)}
           >
             <Trash2Icon className="size-4" />
           </button>
